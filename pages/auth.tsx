19d85113feb0df29c7c98caff5fd6b05a7865d1d
aditya12501 <adityaagrawal12501@gmail.com>
2023-05-16 15:58:59 +0530

    six commit

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -113,7 +113,7 @@ const Auth = () => {
                             <FcGoogle size={30}  />
                         </div>
                         <div onClick={()=>{
-                            signIn('github',{redirect:false,callbackUrl:'/'})
+                            signIn('github',{redirect:true,callbackUrl:'/'})
                             router.push('/profiles');
                             }} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
                             <FaGithub size={30}  />
